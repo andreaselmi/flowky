7cee0360216b8077db357435ab03da673a9cdf69
Andrea Selmi <andreaselmi90@gmail.com>
2025-06-24 18:21:54 +0200

    feat: add storybook to the project

---
+++ src/components/button/button.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+
+import Button from './';
+
+const meta = {
+	component: Button,
+	title: "Button",
+
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Light: Story = {
+	args: {
+		children: "Light button"
+	},
+};
+
+export const Dark: Story = {
+	args: {
+		children: "Dark button"
+	},
+	decorators: [
+		(Story) => (
+			<div className="dark">
+				<Story />
+			</div>
+		)
+	]
+};
